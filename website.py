9d8340fc7e1ffc22bba9ec690332b8b6688e92f4
spainhour <msspainhour@gmail.com>
2018-03-27 20:40:26 -0500

    made some changes, pls don't break
    plssss don’t break

--- website.py
+++ website.py
@@ -1,9 +1,14 @@
+import sqlite3
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
+@app.route('/events')
+def eventsPage():
     return render_template('events.html')
+
+@app.route('/admin')
+def adminPage():
+    return render_template('admin.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
