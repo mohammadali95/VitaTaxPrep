5ea2ad13ee15b6b51aea8f29e0809a4d98089d4e
spainhour <msspainhour@gmail.com>
2018-04-03 14:53:35 -0500

    Got Ali's css and js stuff to work

--- static/main.js
+++ static/main.js
@@ -1,9 +1,21 @@
-$(document).ready(() => {
-  $("#myBtn").on("click", () => {
-    $("#popup").css("display", "block")
-  })
-  
-  $(".close").on("click", () => {
-    $(".modal").css("display", "none")
-  })
-})
+$(document).ready(function(){
+    $("#Info").mouseenter(function(){
+        $(".con").show();
+        $("#Info").addClass('button-active');
+    });
+
+  $(".con").mouseleave(function(){
+        $(".con").hide();
+     $("#Info").removeClass('button-active')
+    });
+
+  $("#Info2").mouseenter(function(){
+        $(".con").show();
+        $("#Info2").addClass('button-active');
+    });
+
+  $(".con").mouseleave(function(){
+        $(".con").hide();
+     $("#Info2").removeClass('button-active')
+    });
+});
