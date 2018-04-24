9c561826b756a278b7a72adecf44af678a4595f5
Blake Hawkins <hawkinsba@hendrix.edu>
2018-04-24 18:34:47 -0500

    Work on events stylesheet

--- static/events.js
+++ static/events.js
@@ -4,13 +4,6 @@ $(document).ready(() => {
     $("#popup").css("display", "block")
   })
 
-// This doesn't work right now, but I'm trying to get the image to grey out and have a "More Details"
-// thing come over the greyed out image on mouseover.
-  $(".event-img").on("mouseover", "*", function(ev) {
-    $(ev.target).addClass("photo-active");
-    return false;
-  });
-
   $(".close").on("click", () => {
     $(".modal").css("display", "none")
   })
