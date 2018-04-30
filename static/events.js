cca9fc332e2397af65730b1814e9585f7560f97c
spainhour <msspainhour@gmail.com>
2018-04-30 12:11:11 -0500

    changes

--- static/events.js
+++ static/events.js
@@ -1,21 +1,18 @@
 $(document).ready(() => {
-
-  $(".event-img").on("click", () => {
-    $("#popup").css("display", "block")
-  })
-  
-  function popup(name, description) {
-	  $("#eventName").textContent = name
-	  $("#description").textContent = description
-	  $("#popup").css("display", "block")
-  }
-
   $(".close").on("click", () => {
     $(".modal").css("display", "none")
   })
 
   $(".submit").on("click", () => {
     $(".modal").css("display", "none")
-  })
 
+  })
+  console.log("Logged");
 })
+
+function popup(name, description) {
+  console.log("You called the popup function!")
+    $("#eventName").html(name)
+    $("#description").html(description)
+    $("#popup").css("display", "block")
+  }
